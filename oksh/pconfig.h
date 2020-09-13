fca143acdf696493dd44635db275bc906e8edd84
Firas Khalil Khana <firasuke@gmail.com>
2020-09-13 23:35:35 +0300

    Revamp cross oksh and add pconfig.h

---
+++ oksh/pconfig.h
@@ -0,0 +1,13 @@
+#define __dead __attribute__((__noreturn__))
+#define HAVE_ASPRINTF
+#define HAVE_CONFSTR
+#define HAVE_CURSES
+#define HAVE_SETRESGID
+#define HAVE_SETRESUID
+#define HAVE_SIG_T
+#define HAVE_ST_MTIM
+#define HAVE_ST_MTIMESPEC
+#define HAVE_STRLCAT
+#define HAVE_STRLCPY
+#define HAVE_TIMERADD
+#define HAVE_TIMERSUB
