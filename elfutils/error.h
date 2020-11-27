d4a576ca11bfd6879fef82ce66d33ac4f64b4d7e
Firas Khalil Khana <firasuke@gmail.com>
2020-11-27 23:59:36 +0200

    Adjust elfutils and elfutils-libelf patches

--- elfutils/error.h
+++ elfutils/error.h
@@ -11,17 +11,17 @@ static unsigned int error_message_count = 0;
 
 static inline void error(int status, int errnum, const char* format, ...)
 {
-  va_list ap;
-  fprintf(stderr, "%s: ", program_invocation_name);
-  va_start(ap, format);
-  vfprintf(stderr, format, ap);
-  va_end(ap);
-  if (errnum)
-    fprintf(stderr, ": %s", strerror(errnum));
-  fprintf(stderr, "\n");
-  error_message_count++;
-  if (status)
-    exit(status);
+	va_list ap;
+	fprintf(stderr, "%s: ", program_invocation_name);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	if (errnum)
+		fprintf(stderr, ": %s", strerror(errnum));
+	fprintf(stderr, "\n");
+	error_message_count++;
+	if (status)
+		exit(status);
 }
 
-#endif
+#endif	/* _ERROR_H_ */
